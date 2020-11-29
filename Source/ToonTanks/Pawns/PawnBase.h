5c21889cc6a585a5d2f90c81f2f8900f28552bde
kevin-brink <kbrink170@gmail.com>
2020-11-29 10:56:27 -0600

    4 - Creating Components

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -14,8 +14,14 @@ class TOONTANKS_API APawnBase : public APawn
 	GENERATED_BODY()
 
 private:
-
+	UPROPERTY()
 	UCapsuleComponent *CapsuleComp;
+	UPROPERTY()
+	UStaticMeshComponent *BaseMesh;
+	UPROPERTY()
+	UStaticMeshComponent *TurretMesh;
+	UPROPERTY()
+	USceneComponent *ProjectileSpawnPoint;
 
 public:
 	// Sets default values for this pawn's properties
