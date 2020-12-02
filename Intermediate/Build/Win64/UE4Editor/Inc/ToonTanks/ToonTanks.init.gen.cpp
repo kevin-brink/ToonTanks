9fb6139287ed236067093c304b19a97a88a3e929
kevin-brink <kbrink170@gmail.com>
2020-12-02 17:27:23 -0600

    14 - Creating Actor Classes

--- Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x4D043747,
-				0x138587E7,
+				0x6F57B82B,
+				0x75FC1632,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
